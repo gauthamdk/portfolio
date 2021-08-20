b8484bd21ad666a44a71446114d4f3254ecc6734
Gautham <gauthamkumar.gk.gk@gmail.com>
2021-08-20 23:12:28 +0000

    feat: add skill bar

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,69 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import "../styles/body.scss";
 
 const Skills = () => {
-  return <Container>Skills</Container>;
+  return (
+    <Container className="px-3 pb-4">
+      <h1 className="secondary_color">Skills</h1>
+      <Row>
+        <Col md={6}>
+          <div class="skill">Python</div>
+          <div class="progress-line Python">
+            <span></span>
+          </div>
+        </Col>
+        <Col md={6}>
+          <div class="skill">JS</div>
+          <div class="progress-line JS">
+            <span></span>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6}>
+          <div class="skill">SQL</div>
+          <div class="progress-line SQL">
+            <span></span>
+          </div>
+        </Col>
+        <Col md={6}>
+          <div class="skill">React</div>
+          <div class="progress-line React">
+            <span></span>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6}>
+          <div class="skill">MongoDB</div>
+          <div class="progress-line MongoDB">
+            <span></span>
+          </div>
+        </Col>
+        <Col md={6}>
+          <div class="skill">NodeJS</div>
+          <div class="progress-line NodeJS">
+            <span></span>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6}>
+          <div class="skill">Docker</div>
+          <div class="progress-line Docker">
+            <span></span>
+          </div>
+        </Col>
+        <Col md={6}>
+          <div class="skill">AWS</div>
+          <div class="progress-line AWS">
+            <span></span>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Skills;
