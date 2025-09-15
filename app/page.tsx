f382f4e270ecb5cbad574b21b9cd17ae5c91b08f
Gautham Dinesh <gauthamkumar.gk.gk@gmail.com>
2025-09-15 22:48:21 +0100

    update projects

--- app/page.tsx
+++ app/page.tsx
@@ -219,9 +219,18 @@ export default function Home() {
 
       {/* Projects Section */}
       <section id="projects" className="mb-8 sm:mb-10">
-        <TerminalPrompt>cat projects/</TerminalPrompt>
+        <TerminalPrompt>ls -ls projects/</TerminalPrompt>
         <div className="command-output">
           <div className="space-y-1">
+            <FileItem
+              permissions="-rwxr-xr-x"
+              size="4.2KB"
+              date="2025"
+              name="movies-in-order"
+              type="executable"
+              href="https://www.moviesinorder.site/"
+            />
+
             <FileItem
               permissions="-rwxr-xr-x"
               size="3.8KB"
@@ -232,8 +241,17 @@ export default function Home() {
 
             <FileItem
               permissions="-rwxr-xr-x"
+              size="2.1KB"
+              date="2021"
+              name="jyun-interactive"
+              type="executable"
+              href="https://jyun-dev.vercel.app/"
+            />
+
+            <FileItem
+              permissions="-rwxr-xr-x"
               size="2.8KB"
-              date="2023"
+              date="2020"
               name="aws-migration-pipeline"
               type="executable"
             />
@@ -241,7 +259,7 @@ export default function Home() {
             <FileItem
               permissions="-rwxr-xr-x"
               size="1.9KB"
-              date="2022"
+              date="2020"
               name="gym-booking-bot"
               type="executable"
               href="https://github.com/gauthamdk/GymBookingAutomation"
@@ -249,20 +267,20 @@ export default function Home() {
 
             <FileItem
               permissions="-rwxr-xr-x"
-              size="1.7KB"
-              date="2022"
-              name="leadandlead"
+              size="1.4KB"
+              date="2020"
+              name="deadliner"
               type="executable"
-              href="https://github.com/gauthamdk/LeadAndLead"
+              href="https://github.com/gauthamdk/Deadliner"
             />
 
             <FileItem
               permissions="-rwxr-xr-x"
-              size="1.4KB"
-              date="2021"
-              name="deadliner"
+              size="1.7KB"
+              date="2019"
+              name="leadandlead"
               type="executable"
-              href="https://github.com/gauthamdk/Deadliner"
+              href="https://github.com/gauthamdk/LeadAndLead"
             />
           </div>
         </div>
