82efd002541f3a2d63fe53e6109fdd0b39dfc907
Gautham Dinesh <gauthamkumar.gk.gk@gmail.com>
2025-09-16 23:04:58 +0100

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,38 +5,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://gautham.dk",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
-    },
-    {
-      url: "https://gautham.dk#about",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://gautham.dk#experience",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://gautham.dk#projects",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.9,
-    },
-    {
-      url: "https://gautham.dk#skills",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
-    {
-      url: "https://gautham.dk#contact",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.6,
     },
   ];
 }
